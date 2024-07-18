e8946c8c0771a6cd7e18ccabf441de2349fd96e8
GXJ <3141316844@qq.com>
2024-07-18 18:44:26 +0800

    Add the duplicate removal function

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r",encoding="utf-8") as fh:
 
 setuptools.setup(
   name="hiscan",
-  version="0.0.5",
+  version="1.2.1",
   author="Yang Xiao",
   author_email="fredrik1999@163.com",
   description="Scanning histone mimics in secquences.",
